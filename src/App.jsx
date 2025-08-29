fd02f7514467e36b8897db2add92350c95aca474
Kaleb Bastos Braz Da Silva <kaleb.braz@gmail.com>
2025-08-28 22:08:25 -0300

    Adicionando App.jsx alterado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -10,27 +9,23 @@ import Contato from "./pages/Contato";
 import Projetos from "./pages/Projetos";
 import SeuSite from './pages/SeuSite';
 
-function App() {
-    return (
-        <Router>
-            <div id="root">
-                <Header />
-                <main className="main-content">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/sobre" element={<Sobre />} />
-                        <Route path="/contato" element={<Contato />} />
-                        <Route path="/faq" element={<FAQ />} />
-                        <Route path="/projetos" element={<Projetos />} />
-                        <Route path="/seusite" element={<SeuSite />} />
-
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
-    );
+export default function App() {
+  return (
+    <Router>
+      <div className="app-shell">
+        <Header />
+        <main className="main-content" id="content" tabIndex={-1}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/projetos" element={<Projetos />} />
+            <Route path="/seusite" element={<SeuSite />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-export default App
-
-
